1d5a82491181a7a320ee1489c7fe633969dec280
aflori <aurelien.flori@le-campus-numerique.fr>
2023-10-24 11:42:17 +0200

    interface

---
+++ output/outputRepository.py
@@ -0,0 +1,14 @@
+
+from Board.board import Board
+
+
+"""
+This class represent mandatory functions to transfert data to front-end interface
+"""
+class OutputRepository:
+
+    """
+    send the board information for specified output (terminal, html, ...)
+    """
+    def showBoard(self, board : Board):
+        pass
