0d27825ff5918685262c4ef9728fc9d33af53fff
aflori <aurelien.flori@le-campus-numerique.fr>
2023-10-24 11:46:08 +0200

    cli interface test 2

--- output/test_affichage.py
+++ output/test_affichage.py
@@ -11,5 +11,15 @@ def test_printBoard():
     exemple_board: Board = Board(numberOfNobles=3, gold=3, red=5, blue=5, black=5, white=5, green=5, card_level_1=3,
                                 card_level_2=6, card_level_3=5)
 
-    text_printed = board_repository.showBoard(exemple_board)
-    assert text_printed == "Board(numberOfNobles=3, gold=3, red=5, green=5, blue=5, black=5, white=5, card_level_3=5, card_level_2=6, card_level_1=3)"
+    actual = board_repository.showBoard(exemple_board)
+    expected = "Board(numberOfNobles=3, gold=3, red=5, green=5, blue=5, black=5, white=5, card_level_3=5, card_level_2=6, card_level_1=3)"
+    assert actual == expected
+
+def test_printBoard2():
+    board_repository: CliInterface = CliInterface()
+    exemple_board: Board = Board(numberOfNobles=3, gold=14, red=5, blue=5, black=5, white=5, green=5, card_level_1=3,
+                                card_level_2=6, card_level_3=5)
+
+    actual = board_repository.showBoard(exemple_board)
+    expected = "Board(numberOfNobles=3, gold=14, red=5, green=5, blue=5, black=5, white=5, card_level_3=5, card_level_2=6, card_level_1=3)"
+    assert actual == expected
