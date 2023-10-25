120384c3d14866610407e723a445c5d5eb68694c
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2023-10-25 11:32:41 +0200

    #9 Implémentation de la règle du minimum de jetons nécessaire pour en prendre deux de la même couleur

--- turn/step_1/action_take_2_coins_of_the_same_color.py
+++ turn/step_1/action_take_2_coins_of_the_same_color.py
@@ -1,23 +1,34 @@
 from Board.board import Board
 from player.Player import Player
+from turn.step_1.Error import NotEnoughCoinsError
 
 
 class TakeTwoCoinsCommand:
-    def __init__(self, game_repository, player_repository):
-        self.game_repository = game_repository
+    def __init__(self, board_repository, player_repository):
+        self.board_repository = board_repository
         self.player_repository = player_repository
 
-    def execute(self, player: Player, coins_color: str):
+    def execute(self, player: Player, coins_color: str, board: Board):
         match coins_color:
             case "green":
+                if board.green < 4:
+                    raise NotEnoughCoinsError()
                 player.green += 2
             case "red":
+                if board.red < 4:
+                    raise NotEnoughCoinsError()
                 player.red += 2
             case "black":
+                if board.black < 4:
+                    raise NotEnoughCoinsError()
                 player.black += 2
             case "blue":
+                if board.blue < 4:
+                    raise NotEnoughCoinsError()
                 player.blue += 2
             case "white":
+                if board.white < 4:
+                    raise NotEnoughCoinsError()
                 player.white += 2
 
         self.player_repository.save(player)
--- turn/step_1/test_action_2_coins.py
+++ turn/step_1/test_action_2_coins.py
@@ -1,18 +1,24 @@
+import pytest
+
+from Board.board import Board
 from player.Player import Player
 from player import playerRepository
 from start_game import gameRepository
+from turn.step_1.Error import NotEnoughCoinsError
 from turn.step_1.action_take_2_coins_of_the_same_color import TakeTwoCoinsCommand
 
 
+# @pytest.mark.skip("test raise exception")
 def test_take_2_green_coins():
     player_repository = playerRepository.PlayerRepositoryInMemory()
     game_repository = gameRepository.GameRepositoryInMemory()
 
-    take_coins_command = TakeTwoCoinsCommand(game_repository=game_repository, player_repository=player_repository)
+    take_coins_command = TakeTwoCoinsCommand(board_repository=game_repository, player_repository=player_repository)
 
     example_player: Player = Player(gold=0, red=0, green=0, blue=0, black=0, white=0)
-
-    take_coins_command.execute(example_player, "green")
+    example_board: Board = Board(blue=4, black=0, green=4, white=1, red=0, gold=0, card_level_1=4, card_level_2=4,
+                                 card_level_3=4, number_of_nobles=5)
+    take_coins_command.execute(example_player, "green", example_board)
 
     actual = player_repository.get_player()
     expected = Player(gold=0, red=0, green=2, blue=0, black=0, white=0)
@@ -20,15 +26,18 @@ def test_take_2_green_coins():
     assert actual == expected
 
 
+# @pytest.mark.skip
 def test_take_2_red_coins():
     player_repository = playerRepository.PlayerRepositoryInMemory()
     game_repository = gameRepository.GameRepositoryInMemory()
 
-    take_coins_command = TakeTwoCoinsCommand(game_repository=game_repository, player_repository=player_repository)
+    take_coins_command = TakeTwoCoinsCommand(board_repository=game_repository, player_repository=player_repository)
 
     example_player: Player = Player(gold=0, red=0, green=0, blue=2, black=1, white=0)
+    example_board: Board = Board(blue=4, black=0, green=4, white=1, red=4, gold=0, card_level_1=4, card_level_2=4,
+                                 card_level_3=4, number_of_nobles=5)
 
-    take_coins_command.execute(example_player, "red")
+    take_coins_command.execute(example_player, "red", example_board)
 
     actual = player_repository.get_player()
     expected = Player(gold=0, red=2, green=0, blue=2, black=1, white=0)
@@ -36,15 +45,18 @@ def test_take_2_red_coins():
     assert actual == expected
 
 
+# @pytest.mark.skip
 def test_take_2_white_coins():
     player_repository = playerRepository.PlayerRepositoryInMemory()
     game_repository = gameRepository.GameRepositoryInMemory()
 
-    take_coins_command = TakeTwoCoinsCommand(game_repository=game_repository, player_repository=player_repository)
+    take_coins_command = TakeTwoCoinsCommand(board_repository=game_repository, player_repository=player_repository)
 
     example_player: Player = Player(gold=0, red=5, green=3, blue=7, black=12, white=3)
+    example_board: Board = Board(blue=4, black=0, green=4, white=5, red=0, gold=0, card_level_1=4, card_level_2=4,
+                                 card_level_3=4, number_of_nobles=5)
 
-    take_coins_command.execute(example_player, "white")
+    take_coins_command.execute(example_player, "white", example_board)
 
     actual = player_repository.get_player()
     expected = Player(gold=0, red=5, green=3, blue=7, black=12, white=5)
@@ -52,15 +64,18 @@ def test_take_2_white_coins():
     assert actual == expected
 
 
+# @pytest.mark.skip
 def test_take_2_blue_coins():
     player_repository = playerRepository.PlayerRepositoryInMemory()
     game_repository = gameRepository.GameRepositoryInMemory()
 
-    take_coins_command = TakeTwoCoinsCommand(game_repository=game_repository, player_repository=player_repository)
+    take_coins_command = TakeTwoCoinsCommand(board_repository=game_repository, player_repository=player_repository)
 
     example_player: Player = Player(gold=2, red=5, green=3, blue=5, black=1, white=5)
+    example_board: Board = Board(blue=4, black=0, green=4, white=1, red=0, gold=0, card_level_1=4, card_level_2=4,
+                                 card_level_3=4, number_of_nobles=5)
 
-    take_coins_command.execute(example_player, "blue")
+    take_coins_command.execute(example_player, "blue", example_board)
 
     actual = player_repository.get_player()
     expected = Player(gold=2, red=5, green=3, blue=7, black=1, white=5)
@@ -68,15 +83,18 @@ def test_take_2_blue_coins():
     assert actual == expected
 
 
+# @pytest.mark.skip
 def test_take_2_black_coins():
     player_repository = playerRepository.PlayerRepositoryInMemory()
     game_repository = gameRepository.GameRepositoryInMemory()
 
-    take_coins_command = TakeTwoCoinsCommand(game_repository=game_repository, player_repository=player_repository)
+    take_coins_command = TakeTwoCoinsCommand(board_repository=game_repository, player_repository=player_repository)
 
     example_player: Player = Player(gold=0, red=1, green=1, blue=2, black=1, white=1)
+    example_board: Board = Board(blue=4, black=4, green=4, white=1, red=0, gold=0, card_level_1=4, card_level_2=4,
+                                 card_level_3=4, number_of_nobles=5)
 
-    take_coins_command.execute(example_player, "black")
+    take_coins_command.execute(example_player, "black", example_board)
 
     actual = player_repository.get_player()
     expected = Player(gold=0, red=1, green=1, blue=2, black=3, white=1)
@@ -84,3 +102,37 @@ def test_take_2_black_coins():
     assert actual == expected
 
 
+def test_4_or_more_tokens_when_take_two():
+    game_repository = gameRepository.GameRepositoryInMemory()
+    player_repository = playerRepository.PlayerRepositoryInMemory()
+
+    example_player: Player = Player(gold=0, red=1, green=1, blue=2, black=1, white=0)
+    example_board: Board = Board(blue=4, black=0, green=3, white=1, red=0, gold=0, card_level_1=4, card_level_2=4,
+                                 card_level_3=4, number_of_nobles=5)
+    take_coins_command = TakeTwoCoinsCommand(board_repository=game_repository, player_repository=player_repository)
+
+    has_not_enough_coin_error = False
+    try:
+        take_coins_command.execute(example_player, "green", example_board)
+    except NotEnoughCoinsError:
+        has_not_enough_coin_error = True
+
+    assert has_not_enough_coin_error
+
+
+def test_4_or_more_tokens_when_take_two_bis():
+    game_repository = gameRepository.GameRepositoryInMemory()
+    player_repository = playerRepository.PlayerRepositoryInMemory()
+
+    example_player: Player = Player(gold=0, red=1, green=1, blue=2, black=1, white=0)
+    example_board: Board = Board(blue=4, black=0, green=3, white=1, red=0, gold=0, card_level_1=4, card_level_2=4,
+                                 card_level_3=4, number_of_nobles=5)
+    take_coins_command = TakeTwoCoinsCommand(board_repository=game_repository, player_repository=player_repository)
+
+    has_not_enough_coin_error = False
+    try:
+        take_coins_command.execute(example_player, "white", example_board)
+    except NotEnoughCoinsError:
+        has_not_enough_coin_error = True
+
+    assert has_not_enough_coin_error
