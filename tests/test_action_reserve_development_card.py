7e71b08725195d1f6c155c6d9370a5c46e713d6a
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2023-10-26 11:52:15 +0200

    close #11 Ajoût des test et des fonctions qui gèrent la réservation de cartes de développement et la prise de joker

--- tests/test_action_reserve_development_card.py
+++ tests/test_action_reserve_development_card.py
@@ -16,7 +16,7 @@ def test_should_player_reserve_exposed_development_card():
 
     example_player: Player = Player(reserved_development_cards=0, gold=0, red=0, green=0, blue=0, black=0, white=0)
     example_board: Board = Board(hidden_development_cards_by_level={1: 36, 2: 26, 3: 16}, blue=4, black=0, green=4,
-                                 white=1, red=0, gold=0, exposed_development_cards_by_level={1: 4, 2: 4, 3: 4},
+                                 white=1, red=0, gold=5, exposed_development_cards_by_level={1: 4, 2: 4, 3: 4},
                                  number_of_nobles=5)
 
     total_development_cards = example_board.exposed_development_cards()
@@ -26,9 +26,9 @@ def test_should_player_reserve_exposed_development_card():
 
     # then
     actual = (player_repository.get_player(), board_repository.get_board())
-    expected = (Player(reserved_development_cards=1, gold=0, red=0, green=0, blue=0, black=0, white=0),
+    expected = (Player(reserved_development_cards=1, gold=1, red=0, green=0, blue=0, black=0, white=0),
                 Board(hidden_development_cards_by_level={1: 35, 2: 26, 3: 16}, blue=4, black=0, green=4, white=1, red=0,
-                      gold=0,
+                      gold=4,
                       exposed_development_cards_by_level={1: 4, 2: 4, 3: 4}, number_of_nobles=5))
     assert actual == expected
 
@@ -41,7 +41,7 @@ def test_should_player_reserve_hidden_development_card():
     reserve_exposed_development_card = ReserveDevelopmentCardCommand(board_repository=board_repository,
                                                                      player_repository=player_repository)
 
-    example_player: Player = Player(reserved_development_cards=2, gold=0, red=0, green=0, blue=0, black=0, white=0)
+    example_player: Player = Player(reserved_development_cards=2, gold=2, red=0, green=0, blue=0, black=0, white=0)
     example_board: Board = Board(hidden_development_cards_by_level={1: 36, 2: 26, 3: 16}, blue=4, black=0, green=4,
                                  white=1, red=0, gold=0, exposed_development_cards_by_level={1: 4, 2: 4, 3: 4},
                                  number_of_nobles=5)
@@ -53,8 +53,37 @@ def test_should_player_reserve_hidden_development_card():
 
     # then
     actual = (player_repository.get_player(), board_repository.get_board())
-    expected = (Player(reserved_development_cards=3, gold=0, red=0, green=0, blue=0, black=0, white=0),
+    expected = (Player(reserved_development_cards=3, gold=2, red=0, green=0, blue=0, black=0, white=0),
                 Board(hidden_development_cards_by_level={1: 36, 2: 26, 3: 15}, blue=4, black=0, green=4, white=1, red=0,
+                      gold=0,
+                      number_of_nobles=5, exposed_development_cards_by_level={1: 4, 2: 4, 3: 4}))
+
+    assert actual == expected
+
+
+def test_should_player_reserve_hidden_development_card2():
+    # given
+    board_repository = BoardRepositoryInMemory()
+    player_repository = PlayerRepositoryInMemory()
+
+    reserve_exposed_development_card = ReserveDevelopmentCardCommand(board_repository=board_repository,
+                                                                     player_repository=player_repository)
+
+    example_player: Player = Player(reserved_development_cards=2, gold=2, red=0, green=0, blue=0, black=0, white=0)
+    example_board: Board = Board(hidden_development_cards_by_level={1: 36, 2: 26, 3: 16}, blue=4, black=0, green=4,
+                                 white=1, red=0, gold=1, exposed_development_cards_by_level={1: 4, 2: 4, 3: 4},
+                                 number_of_nobles=5)
+
+    total_development_cards = example_board.exposed_development_cards()
+
+    # when
+    reserve_exposed_development_card.execute(example_player, example_board, "hidden", 2)
+
+    # then
+    actual = (player_repository.get_player(), board_repository.get_board())
+    expected = (Player(reserved_development_cards=3, gold=3, red=0, green=0, blue=0, black=0, white=0),
+                Board(hidden_development_cards_by_level={1: 36, 2: 25, 3: 16}, blue=4, black=0, green=4, white=1,
+                      red=0,
                       gold=0,
                       number_of_nobles=5, exposed_development_cards_by_level={1: 4, 2: 4, 3: 4}))
 
@@ -80,6 +109,8 @@ def test_player_has_three_reserved_cards_exposed():
 
     # then
     assert has_error_already_has_three_reserved_cards
+
+
 def test_player_has_three_reserved_cards_hidden():
     # given
     board_repository = BoardRepositoryInMemory()
--- turn/step_1/action_reserve_development_card.py
+++ turn/step_1/action_reserve_development_card.py
@@ -11,15 +11,21 @@ class ReserveDevelopmentCardCommand:
     def execute(self, player: Player, board: Board, card_type: str, level_card: int):
         if player.reserved_development_cards == 3:
             raise AlreadyThreeReservedCard()
+
         match card_type:
             case "hidden":
                 player.reserved_development_cards += 1
                 board.hidden_development_cards_by_level[level_card] -= 1
+
             case "exposed":
                 board.exposed_development_cards_by_level[level_card] -= 1
                 player.reserved_development_cards += 1
                 board.hidden_development_cards_by_level[level_card] -= 1
                 board.exposed_development_cards_by_level[level_card] += 1
+
+        if board.gold > 0:
+            player.gold += 1
+            board.gold -= 1
 
         self.player_repository.save(player)
         self.board_repository.save(board)
