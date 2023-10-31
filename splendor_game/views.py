4587d9a010c6cded7fedf910cc6716f0a9d83fe0
aflori <aurelien.flori@le-campus-numerique.fr>
2023-10-31 09:19:29 +0100

    small refacto

--- splendor_game/views.py
+++ splendor_game/views.py
@@ -18,11 +18,12 @@ player_repository = PlayerRepositoryInMemory()
 def getGame(request):
     # game_repository = GameRepositoryInMemory()
     start_game = StartGameCommand(game_repository)
-    start_game.execute(2)
+    number_of_player = 2
+    start_game.execute(number_of_player)
     game = game_repository.get_game()
     template = loader.get_template("get_game.html")
     player_repository.save(game.players)
-    context = {"number_of_players": 2, "number_card_exposed_per_level": game.board.exposed_development_cards_by_level,
+    context = {"number_of_players": number_of_player, "number_card_exposed_per_level": game.board.exposed_development_cards_by_level,
                "number_of_nobles": game.board.number_of_nobles, "gold_coin": game.board.gold,
                "red_coins": game.board.red, "green_coins": game.board.green, "blue_coins": game.board.blue,
                "white_coins": game.board.white, "black_coins": game.board.black}
